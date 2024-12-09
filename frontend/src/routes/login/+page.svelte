52afd3efde43f00841fac7f0effc23bd985fce19
borelli28 <borelliarmando28@gmail.com>
2024-12-08 21:29:14 -0500

    Moved login page to its own route

---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+  let username = '';
+  let password = '';
+
+  function handleSubmit() {
+    // Here you would typically handle the registration logic
+    console.log('Registration attempted', { username, password, confirmPassword });
+    // You can add your authentication logic here
+  }
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="/css/login.css">
+	<title>Login</title>
+</svelte:head>
+
+<main>
+	<div id="container">
+		<h1>Login</h1>
+		<form on:submit|preventDefault={handleSubmit}>
+		<div>
+		  <label for="username">Username:</label>
+		  <input type="text" id="username" bind:value={username} required>
+		</div>
+		<div>
+		  <label for="password">Password:</label>
+		  <input type="password" id="password" bind:value={password} required>
+		</div>
+		<button type="submit">Log In</button>
+		</form>
+		<p>New to our platform? <a href="/register">Register here</a></p>
+		<p>Visit <a href="https://svelte.dev/docs/kit" target="_blank">svelte.dev/docs/kit</a> to read the documentation</p>
+	</div>
+</main>
