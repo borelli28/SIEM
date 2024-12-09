2ccd5449b9b1de1e623bfff63d0039fb92987e09
borelli28 <borelliarmando28@gmail.com>
2024-12-08 22:56:10 -0500

    Added basic routing server in main.rs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, HttpResponse, Responder};
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+        .service(
+            web::scope("/backend")
+                .route("/", web::get().to(index))
+        )
+    })
+    .bind(("127.0.0.1", 4200)).expect("Failed to bind to address").run().await
 }
