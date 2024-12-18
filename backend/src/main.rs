b19067b2db669a66847818bc3065f922d8fdec17
borelli28 <borelliarmando28@gmail.com>
2024-12-18 14:52:01 -0500

    Added route and handler method for retrieving all alerts

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -11,6 +11,7 @@ mod schema;
 use actix_web::{web, App, HttpServer, HttpResponse, Responder};
 use crate::collector::{LogCollector, ParseLogError, process_logs};
 use crate::batch_maker::{create_batch};
+use crate::alert::{list_alerts};
 use serde_json::json;
 
 async fn index() -> impl Responder {
@@ -44,6 +45,16 @@ async fn import_log(collector: web::Data<LogCollector>, log: web::Json<String>, 
     }
 }
 
+async fn get_all_alerts(account_id: web::Path<String>) -> impl Responder {
+    match list_alerts(&account_id) {
+        Ok(alerts) => HttpResponse::Ok().json(alerts),
+        Err(err) => HttpResponse::InternalServerError().json(json!({
+            "status": "error",
+            "message": err.to_string()
+        }))
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let collector = web::Data::new(LogCollector::new());
@@ -55,6 +66,10 @@ async fn main() -> std::io::Result<()> {
                 web::scope("/backend")
                     .route("/", web::get().to(index))
                     .route("/import_log", web::post().to(import_log))
+                    .service(
+                        web::scope("/alert")
+                            .route("/get_all/{account_id}", web::get().to(get_all_alerts))
+                    )
             )
     })
     .bind(("127.0.0.1", 4200))?
