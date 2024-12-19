0a751f9a757032875958e2fa01bdd001f2cf25c4
borelli28 <borelliarmando28@gmail.com>
2024-12-19 14:39:53 -0500

    Added host routes in main.rs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -8,6 +8,7 @@ mod rules;
 mod alert;
 mod schema;
 mod handlers;
+mod host;
 
 use actix_web::{web, App, HttpServer};
 use crate::collector::LogCollector;
@@ -17,7 +18,12 @@ use crate::handlers::{
     get_alert_handler,
     get_all_alerts_handler,
     delete_alert_handler,
-    acknowledge_alert_handler
+    acknowledge_alert_handler,
+    create_host_handler,
+    get_host_handler,
+    get_all_hosts_handler,
+    edit_host_handler,
+    delete_host_handler
 };
 
 #[actix_web::main]
@@ -37,6 +43,14 @@ async fn main() -> std::io::Result<()> {
                             .route("/get_all/{account_id}", web::get().to(get_all_alerts_handler))
                             .route("/delete/{alert_id}", web::delete().to(delete_alert_handler))
                             .route("/acknowledge/{alert_id}", web::put().to(acknowledge_alert_handler))
+                    )
+                    .service(
+                        web::scope("/host")
+                            .route("/{account_id}", web::post().to(create_host_handler))
+                            .route("/get/{host_id}", web::get().to(get_host_handler))
+                            .route("/get_all/{account_id}", web::get().to(get_all_hosts_handler))
+                            .route("/edit/{host_id}", web::put().to(edit_host_handler))
+                            .route("/delete/{host_id}", web::delete().to(delete_host_handler))
                     )
             )
     })
