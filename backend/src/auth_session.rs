d5103a4e51783b7008f76109accfc3d195d12686
borelli28 <borelliarmando28@gmail.com>
2024-12-25 15:12:02 -0500

    Fixed auth_sessions methods to work correctly with the session middleware

--- backend/src/auth_session.rs
+++ backend/src/auth_session.rs
@@ -1,5 +1,6 @@
 use actix_web::{dev::Payload, error::ErrorUnauthorized, Error, FromRequest, HttpRequest};
 use actix_session::{Session, SessionExt};
+use std::time::{Duration, SystemTime};
 use std::future::{ready, Ready};
 
 pub struct AuthSession {
@@ -12,19 +13,46 @@ impl FromRequest for AuthSession {
 
     fn from_request(req: &HttpRequest, _: &mut Payload) -> Self::Future {
         let session = req.get_session();
-        match session.get::<String>("session_id") {
-            Ok(Some(account_id)) => ready(Ok(AuthSession { account_id })),
-            _ => ready(Err(ErrorUnauthorized("Unauthorized")))
+        match verify_session(&session) {
+            Ok(account_id) => ready(Ok(AuthSession { account_id })),
+            Err(e) => ready(Err(e)),
         }
     }
 }
 
 pub fn verify_session(session: &Session) -> Result<String, Error> {
-    session.get::<String>("session_id")
-        .map_err(|_| ErrorUnauthorized("Session error"))?
-        .ok_or_else(|| ErrorUnauthorized("Unauthorized"))
+    match session.get::<String>("account_id") {
+        Ok(Some(account_id)) => {
+            // Check if the last_activity is set
+            match session.get::<SystemTime>("last_activity") {
+                Ok(Some(last_activity)) => {
+                    let session_timeout = Duration::from_secs(20 * 60);
+                    // Check if the last activity was more than 20 minutes ago
+                    if SystemTime::now().duration_since(last_activity).unwrap_or(Duration::from_secs(0)) > session_timeout {
+                        session.purge();
+                        return Err(ErrorUnauthorized("Session timeout"));
+                    }
+                },
+                Ok(None) => {
+                    // If last_activity is None
+                    return Err(ErrorUnauthorized("Unauthorized")); 
+                },
+                Err(_) => {
+                    // Error in retrieving last_activity
+                    return Err(ErrorUnauthorized("Session error"));
+                }
+            }
+
+            session.renew();
+            session.insert("last_activity", SystemTime::now())
+                .map_err(|_| ErrorUnauthorized("Failed to update session"))?;
+
+            Ok(account_id)
+        },
+        _ => Err(ErrorUnauthorized("Unauthorized")),
+    }
 }
 
 pub fn invalidate_session(session: &Session) {
-    session.remove("session_id");
+    session.purge();
 }
