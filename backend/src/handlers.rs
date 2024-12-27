07def3100c5b9be13832cbef09f46a25dab50479
borelli28 <borelliarmando28@gmail.com>
2024-12-26 20:24:00 -0500

    Added CSRF validation check in all POST, PUT, & DELETE route handlers

--- backend/src/handlers.rs
+++ backend/src/handlers.rs
@@ -17,55 +17,54 @@ pub async fn index() -> impl Responder {
 
 pub async fn verify_session_handler(session: Session, req: HttpRequest) -> impl Responder {
     match verify_session(&session, &req) {
-        Ok(account_id) => HttpResponse::Ok().json(serde_json::json!({
-            "authenticated": true,
-            "account_id": account_id
-        })),
-        Err(_) => HttpResponse::Unauthorized().json(serde_json::json!({
-            "authenticated": false,
-            "message": "Not authenticated"
-        }))
+        Ok(account_id) => HttpResponse::Ok().json(serde_json::json!({ "authenticated": true, "account_id": account_id })),
+        Err(_) => HttpResponse::Unauthorized().json(serde_json::json!({ "authenticated": false, "message": "Not authenticated" }))
     }
 }
 
 pub async fn logout_handler(session: Session) -> impl Responder {
     invalidate_session(&session);
-    HttpResponse::Ok().json(serde_json::json!({
-        "message": "Logged out successfully"
-    }))
+    HttpResponse::Ok().json(serde_json::json!({ "message": "Logged out successfully" }))
 }
 
 // Logs Handlers
 //
 pub async fn import_log_handler(
-    collector: web::Data<LogCollector>, 
-    log: web::Json<String>, 
-    account_id: web::Json<String>, 
+    req: HttpRequest,
+    csrf: web::Data<CsrfMiddleware>,
+    collector: web::Data<LogCollector>,
+    log: web::Json<String>,
+    account_id: web::Json<String>,
     host_id: web::Json<String>
-    ) -> impl Responder {
+) -> Result<HttpResponse, Error> {
+    csrf_validator(&req, &csrf).await?;
+    
     match create_batch(&log.into_inner()).await {
         Ok(_) => {
             match process_logs(&collector, account_id.to_string(), host_id.to_string()).await {
-                Ok(_) => HttpResponse::Ok().json(json!({ "status": "ok" })),
+                Ok(_) => Ok(HttpResponse::Ok().json(json!({ "status": "ok" }))),
                 Err(e) => {
                     eprintln!("Error processing logs: {}", e);
                     match e {
                         ParseLogError::DatabaseError(msg) => {
-                            HttpResponse::InternalServerError().json(json!({
+                            Ok(HttpResponse::InternalServerError().json(json!({
                                 "status": "error",
                                 "message": "Database error",
                                 "details": msg
-                            }))
+                            })))
                         },
-                        _ => HttpResponse::InternalServerError().json(json!({
+                        _ => Ok(HttpResponse::InternalServerError().json(json!({
                             "status": "error",
                             "message": "Failed to process logs"
-                        }))
+                        })))
                     }
                 }
             }
         },
-        Err(_) => HttpResponse::BadRequest().json(json!({ "status": "error", "message": "Invalid log format" })),
+        Err(_) => Ok(HttpResponse::BadRequest().json(json!({
+            "status": "error",
+            "message": "Invalid log format"
+        }))),
     }
 }
 
@@ -101,35 +100,51 @@ pub async fn get_all_alerts_handler(account_id: web::Path<String>) -> impl Respo
     }
 }
 
-pub async fn delete_alert_handler(alert_id: web::Path<String>) -> impl Responder {
+pub async fn delete_alert_handler(
+    req: HttpRequest,
+    alert_id: web::Path<String>,
+    csrf: web::Data<CsrfMiddleware>
+) -> Result<HttpResponse, Error> {
+    csrf_validator(&req, &csrf).await?;
     match delete_alert(&alert_id.to_string()) {
-        Ok(ok) => HttpResponse::Ok().json(ok),
-        Err(err) => HttpResponse::InternalServerError().json(json!({
+        Ok(ok) => Ok(HttpResponse::Ok().json(ok)),
+        Err(err) => Ok(HttpResponse::InternalServerError().json(json!({
             "status": "error",
             "message": err.to_string()
-        }))
+        })))
     }
 }
 
-pub async fn acknowledge_alert_handler(alert_id: web::Path<String>) -> impl Responder {
+pub async fn acknowledge_alert_handler(
+    req: HttpRequest,
+    alert_id: web::Path<String>,
+    csrf: web::Data<CsrfMiddleware>
+) -> Result<HttpResponse, Error> {
+    csrf_validator(&req, &csrf).await?;
     match acknowledge_alert(&alert_id.to_string()) {
-        Ok(ok) => HttpResponse::Ok().json(ok),
-        Err(err) => HttpResponse::InternalServerError().json(json!({
+        Ok(ok) => Ok(HttpResponse::Ok().json(ok)),
+        Err(err) => Ok(HttpResponse::InternalServerError().json(json!({
             "status": "error",
             "message": err.to_string()
-        }))
+        })))
     }
 }
 
 // Host Handlers
 //
-pub async fn create_host_handler(host: web::Json<Host>, account_id: web::Path<String>) -> impl Responder {
+pub async fn create_host_handler(
+    req: HttpRequest,
+    host: web::Json<Host>,
+    account_id: web::Path<String>,
+    csrf: web::Data<CsrfMiddleware>
+) -> Result<HttpResponse, Error> {
+    csrf_validator(&req, &csrf).await?;
     match create_host(&host, &account_id.to_string()) {
-        Ok(host) => HttpResponse::Ok().json(host),
-        Err(err) => HttpResponse::InternalServerError().json(json!({
+        Ok(host) => Ok(HttpResponse::Ok().json(host)),
+        Err(err) => Ok(HttpResponse::InternalServerError().json(json!({
             "status": "error",
             "message": err.to_string()
-        }))
+        })))
     }
 }
 
@@ -153,35 +168,50 @@ pub async fn get_all_hosts_handler(account_id: web::Path<String>) -> impl Respon
     }
 }
 
-pub async fn edit_host_handler(host: web::Json<Host>) -> impl Responder {
+pub async fn edit_host_handler(
+    req: HttpRequest,
+    host: web::Json<Host>,
+    csrf: web::Data<CsrfMiddleware>
+) -> Result<HttpResponse, Error> {
+    csrf_validator(&req, &csrf).await?;
     match update_host(&host) {
-        Ok(ok) => HttpResponse::Ok().json(ok),
-        Err(err) => HttpResponse::InternalServerError().json(json!({
+        Ok(ok) => Ok(HttpResponse::Ok().json(ok)),
+        Err(err) => Ok(HttpResponse::InternalServerError().json(json!({
             "status": "error",
             "message": err.to_string()
-        }))
+        })))
     }
 }
 
-pub async fn delete_host_handler(host_id: web::Path<String>) -> impl Responder {
+pub async fn delete_host_handler(
+    req: HttpRequest,
+    host_id: web::Path<String>,
+    csrf: web::Data<CsrfMiddleware>
+) -> Result<HttpResponse, Error> {
+    csrf_validator(&req, &csrf).await?;
     match delete_host(&host_id.to_string()) {
-        Ok(ok) => HttpResponse::Ok().json(ok),
-        Err(err) => HttpResponse::InternalServerError().json(json!({
+        Ok(ok) => Ok(HttpResponse::Ok().json(ok)),
+        Err(err) => Ok(HttpResponse::InternalServerError().json(json!({
             "status": "error",
             "message": err.to_string()
-        }))
+        })))
     }
 }
 
-// Host Handlers
+// Rule Handlers
 //
-pub async fn create_rule_handler(rule: web::Json<AlertRule>) -> impl Responder {
+pub async fn create_rule_handler(
+    req: HttpRequest,
+    rule: web::Json<AlertRule>,
+    csrf: web::Data<CsrfMiddleware>
+) -> Result<HttpResponse, Error> {
+    csrf_validator(&req, &csrf).await?;
     match create_rule(&rule) {
-        Ok(ok) => HttpResponse::Ok().json(ok),
-        Err(err) => HttpResponse::InternalServerError().json(json!({
+        Ok(ok) => Ok(HttpResponse::Ok().json(ok)),
+        Err(err) => Ok(HttpResponse::InternalServerError().json(json!({
             "status": "error",
             "message": err.to_string()
-        }))
+        })))
     }
 }
 
@@ -205,49 +235,63 @@ pub async fn get_all_rules_handler(account_id: web::Path<String>) -> impl Respon
     }
 }
 
-pub async fn edit_rule_handler(rule: web::Json<AlertRule>) -> impl Responder {
+pub async fn edit_rule_handler(
+    req: HttpRequest,
+    rule: web::Json<AlertRule>,
+    csrf: web::Data<CsrfMiddleware>
+) -> Result<HttpResponse, Error> {
+    csrf_validator(&req, &csrf).await?;
     match update_rule(&rule) {
-        Ok(ok) => HttpResponse::Ok().json(ok),
-        Err(err) => HttpResponse::InternalServerError().json(json!({
+        Ok(ok) => Ok(HttpResponse::Ok().json(ok)),
+        Err(err) => Ok(HttpResponse::InternalServerError().json(json!({
             "status": "error",
             "message": err.to_string()
-        }))
+        })))
     }
 }
 
-pub async fn delete_rule_handler(rule_id: web::Path<String>) -> impl Responder {
+pub async fn delete_rule_handler(
+    req: HttpRequest,
+    rule_id: web::Path<String>,
+    csrf: web::Data<CsrfMiddleware>
+) -> Result<HttpResponse, Error> {
+    csrf_validator(&req, &csrf).await?;
     match delete_rule(&rule_id.to_string()) {
-        Ok(ok) => HttpResponse::Ok().json(ok),
-        Err(err) => HttpResponse::InternalServerError().json(json!({
+        Ok(ok) => Ok(HttpResponse::Ok().json(ok)),
+        Err(err) => Ok(HttpResponse::InternalServerError().json(json!({
             "status": "error",
             "message": err.to_string()
-        }))
+        })))
     }
 }
 
 // Account Handlers
 //
-pub async fn create_account_handler(account: web::Json<Account>) -> impl Responder {
+pub async fn create_account_handler(
+    req: HttpRequest,
+    account: web::Json<Account>,
+    csrf: web::Data<CsrfMiddleware>
+) -> Result<HttpResponse, Error> {
+    csrf_validator(&req, &csrf).await?;
     let account = account.into_inner();
     let name = account.name;
     let password = account.password;
     let role = account.role;
-
     match create_account(name, password, role) {
-        Ok(account) => HttpResponse::Ok().json(account),
+        Ok(account) => Ok(HttpResponse::Ok().json(account)),
         Err(err) => match err {
-            AccountError::InvalidRole => HttpResponse::BadRequest().json(json!({
+            AccountError::InvalidRole => Ok(HttpResponse::BadRequest().json(json!({
                 "status": "error",
                 "message": "Invalid role provided"
-            })),
-            AccountError::ExpectedField(field) => HttpResponse::BadRequest().json(json!({
+            }))),
+            AccountError::ExpectedField(field) => Ok(HttpResponse::BadRequest().json(json!({
                 "status": "error",
                 "message": format!("Missing required field: {}", field)
-            })),
-            _ => HttpResponse::InternalServerError().json(json!({
+            }))),
+            _ => Ok(HttpResponse::InternalServerError().json(json!({
                 "status": "error",
                 "message": "An internal error occurred"
-            }))
+            })))
         }
     }
 }
@@ -262,23 +306,33 @@ pub async fn get_account_handler(account_id: web::Path<String>) -> impl Responde
     }
 }
 
-pub async fn edit_account_handler(account: web::Json<Account>) -> impl Responder {
+pub async fn edit_account_handler(
+    req: HttpRequest,
+    account: web::Json<Account>,
+    csrf: web::Data<CsrfMiddleware>
+) -> Result<HttpResponse, Error> {
+    csrf_validator(&req, &csrf).await?;
     match update_account(&account) {
-        Ok(ok) => HttpResponse::Ok().json(ok),
-        Err(err) => HttpResponse::InternalServerError().json(json!({
+        Ok(ok) => Ok(HttpResponse::Ok().json(ok)),
+        Err(err) => Ok(HttpResponse::InternalServerError().json(json!({
             "status": "error",
             "message": err.to_string()
-        }))
+        })))
     }
 }
 
-pub async fn delete_account_handler(account_id: web::Path<String>) -> impl Responder {
+pub async fn delete_account_handler(
+    req: HttpRequest,
+    account_id: web::Path<String>,
+    csrf: web::Data<CsrfMiddleware>
+) -> Result<HttpResponse, Error> {
+    csrf_validator(&req, &csrf).await?;
     match delete_account(&account_id.to_string()) {
-        Ok(ok) => HttpResponse::Ok().json(ok),
-        Err(err) => HttpResponse::InternalServerError().json(json!({
+        Ok(ok) => Ok(HttpResponse::Ok().json(ok)),
+        Err(err) => Ok(HttpResponse::InternalServerError().json(json!({
             "status": "error",
             "message": err.to_string()
-        }))
+        })))
     }
 }
 
@@ -286,7 +340,6 @@ pub async fn login_account_handler(session: Session, account: web::Json<Account>
     let account_data = account.into_inner();
     let name = account_data.name;
     let password = account_data.password;
-
     match verify_login(&session, &name, &password, &req) {
         Ok(Some(account)) => HttpResponse::Ok().json(json!({
             "status": "success",
