e07459a47748859e11fec03d3632ba34d5a940fb
borelli28 <borelliarmando28@gmail.com>
2024-12-28 02:06:20 -0500

    Fixed register account method by adding role, id, formId, and csrf token

--- frontend/public/js/register.js
+++ frontend/public/js/register.js
@@ -1,8 +1,11 @@
+import { getCsrfToken } from '../../services/csrfService.js';
+
 async function handleSubmit(event) {
     event.preventDefault();
-    const username = document.getElementById('username').value;
+    const name = document.getElementById('name').value;
     const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('confirmPassword').value;
+    const formId = 'register-form'
 
     const alertContainer = document.getElementById('alert-container');
 
@@ -11,22 +14,30 @@ async function handleSubmit(event) {
         return;
     }
 
-    const newAccount = { name: username, password: password };
+    const newAccount = { id: '0', name: name, password: password, role: 'Admin'};
 
     try {
+        const csrfToken = await getCsrfToken(formId);
+
         const response = await fetch('http://localhost:4200/backend/account/', {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Form-ID': formId,
+                'X-XSRF-TOKEN': csrfToken
+            },
             body: JSON.stringify(newAccount),
+            credentials: 'include'
         });
 
         if (response.ok) {
             alertContainer.innerHTML = `<div class="alert success">Account created successfully!</div>`;
-            document.getElementById('username').value = '';
+            document.getElementById('name').value = '';
             document.getElementById('password').value = '';
             document.getElementById('confirmPassword').value = '';
         } else {
             const errorData = await response.json();
+            console.log(errorData);
             alertContainer.innerHTML = `<div class="alert error">Error: ${errorData.message || 'Failed to create account.'}</div>`;
         }
     } catch (error) {
