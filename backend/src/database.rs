1ec44d5d48b19c719ccc6a91c3c76d1a45cb340e
borelli28 <borelliarmando28@gmail.com>
2024-12-31 14:45:24 -0500

    Replaced diesel for rusqlite in database.rs

--- backend/src/database.rs
+++ backend/src/database.rs
@@ -1,11 +1,10 @@
-use diesel::prelude::*;
-use diesel::sqlite::SqliteConnection;
+use rusqlite::{Connection, Result};
 use dotenvy::dotenv;
 use std::env;
 
-pub fn establish_connection() -> SqliteConnection {
+pub fn establish_connection() -> Result<Connection> {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    SqliteConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+    Connection::open(&database_url)
+        .map_err(|_| panic!("Error connecting to {}", database_url))
 }
