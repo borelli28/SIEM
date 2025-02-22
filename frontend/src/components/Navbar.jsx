925ae64d3e87678b1f9da25f3afde9b04b464cad
borelli <borelliarmando28@gmail.com>
2025-02-22 00:03:36 -0500

    Minor change in Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -23,13 +23,13 @@ const Navbar = () => {
     };
 
     return (
-        <div className="container">
+        <div className="nav-container">
             <nav>
-                <a href="/dashboard">Dashboard</a>
-                <a href="/settings">Settings</a>
-                <a href="/alerts">Alerts</a>
-                <a href="/search">Search</a>
-                <a href="/list-cases">Cases</a>
+                <a href="/dashboard" className="primary-btn">Dashboard</a>
+                <a href="/settings" className="primary-btn">Settings</a>
+                <a href="/alerts" className="primary-btn">Alerts</a>
+                <a href="/search" className="primary-btn">Search</a>
+                <a href="/list-cases" className="primary-btn">Cases</a>
                 <a className="danger-btn" onClick={handleLogout}>Logout</a>
             </nav>
         </div>
